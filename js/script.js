5043c6af33e7115b5925baff7cedb8c96869c0d2
MJ Otto <realmjotto@gmail.com>
2019-02-13 09:12:01 +0200

    upload all files to master

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var toggler = document.querySelector('.menu-toggle');
+var menu = document.querySelector('.menu');
+var mainMenuItems = document.querySelectorAll('.main-menu-item');
+var sub = document.querySelectorAll('.sub-menu');
+
+
+window.addEventListener('resize', function(){
+    if(window.innerWidth <= 850){
+        //Toggling the menu on and off 
+        toggler.addEventListener('click',function(){
+        if (menu.style.visibility == 'visible') {
+            menu.style.visibility = 'hidden';
+        } else {
+            menu.style.visibility = 'visible';
+        }
+    });
+
+//Displaying sub menu items on click with a loop
+    mainMenuItems.forEach(function(menuItem){
+        menuItem.addEventListener('click',function(){
+            var x = menuItem.nextElementSibling;
+            x.classList.toggle('sub-menu');
+            x.classList.toggle('open');
+        });
+        })
+    }
+});
+
+
+
+
+
+
+
+
+
---
+++ js/slide.js
@@ -0,0 +1,48 @@
+// const imgSlideContainer = document.querySelector('.hero-images-container');
+// const slideImages = document.querySelectorAll('.hero-images-container img');
+
+//Counter
+// let counter = 1;
+// const size = slideImages[0].clientWidth;
+
+// imgSlideContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Buttons
+// const prevBtn = document.querySelector('.toggle-left');
+// const nextBtn = document.querySelector('.toggle-right');
+
+//Button Listeners
+// nextBtn.addEventListener('click', function(){
+//     imgSlideContainer.style.transition = 'transform 0.4 ease-in-out';
+//     counter++;
+//     imgSlideContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// });
+
+// prevBtn.addEventListener('click', function(){
+//     imgSlideContainer.style.transition = 'transform 0.4 ease-in-out';
+//     counter--;
+//     imgSlideContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+// });
+
+
+//Loop back after last image
+// imgSlideContainer.addEventListener('transitionend', function(){
+//     console.log(slideImages[counter]);
+//     if (slideImages[counter].id == 'lastClone'){
+//         console.log('lastClone');
+//         imgSlideContainer.style.transition = 'none';
+//         counter = slideImages.length -2;
+//         imgSlideContainer.style.transform = 'translateX(' + (-size * counter) + 'px)';
+//     }
+// });
+
+
+
+$(document).ready(function(){
+    $('.hero-images-container').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: (3000),
+        arrows: false,
+    });
+  });
